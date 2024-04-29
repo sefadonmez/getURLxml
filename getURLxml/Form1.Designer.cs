59a11c35fca5a8447dced597648531abc00faff5
sefadonmez <sefadonmez@hotmail.com>
2024-04-29 23:55:32 +0300

    Add project files.

---
+++ getURLxml/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace getURLxml
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewProducts = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.loadXMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProducts)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridViewProducts
+            // 
+            this.dataGridViewProducts.AllowUserToAddRows = false;
+            this.dataGridViewProducts.AllowUserToDeleteRows = false;
+            this.dataGridViewProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProducts.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewProducts.Location = new System.Drawing.Point(0, 24);
+            this.dataGridViewProducts.Name = "dataGridViewProducts";
+            this.dataGridViewProducts.ReadOnly = true;
+            this.dataGridViewProducts.Size = new System.Drawing.Size(1027, 624);
+            this.dataGridViewProducts.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadXMLToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1027, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // loadXMLToolStripMenuItem
+            // 
+            this.loadXMLToolStripMenuItem.Name = "loadXMLToolStripMenuItem";
+            this.loadXMLToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.loadXMLToolStripMenuItem.Text = "Load XML";
+            this.loadXMLToolStripMenuItem.Click += new System.EventHandler(this.loadXMLToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 648);
+            this.Controls.Add(this.dataGridViewProducts);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProducts)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewProducts;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem loadXMLToolStripMenuItem;
+    }
+}
+
---
+++ getURLxml/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+namespace getURLxml
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadXML()
+        {
+            string url = "https://www.yenitoptanci.com/TicimaxXmlV2/82319370DB1A4D88B463B4DD60599779/";
+
+            try
+            {
+                // XML verilerini yüklemek için XmlDocument oluştur
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(url);
+
+                // DataGridView'i temizle
+                dataGridViewProducts.Rows.Clear();
+                dataGridViewProducts.Columns.Clear();
+
+                // XML verilerini DataGridView'e ekle
+                XmlNodeList products = xmlDoc.SelectNodes("//Root//Urunler//Urun");
+                foreach (XmlNode product in products)
+                {
+                    if (dataGridViewProducts.Columns.Count == 0)
+                    {
+                        // İlk satırı sütun başlıkları olarak ekle
+                        foreach (XmlNode node in product.ChildNodes)
+                        {
+                            dataGridViewProducts.Columns.Add(node.Name, node.Name);
+                        }
+                    }
+
+                    // Satırı DataGridView'e ekle
+                    DataGridViewRow dgvRow = new DataGridViewRow();
+                    dgvRow.CreateCells(dataGridViewProducts);
+
+                    int i = 0;
+                    foreach (XmlNode node in product.ChildNodes)
+                    {
+                        dgvRow.Cells[i].Value = node.InnerText;
+                        i++;
+                    }
+
+                    dataGridViewProducts.Rows.Add(dgvRow);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Hata durumunda hata mesajını göster
+                MessageBox.Show("Hata: " + ex.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadXML();
+        }
+
+        private void loadXMLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoadXML();
+        }
+    }
+}
